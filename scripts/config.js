e192c496e1208e1ea2b85e9379a7c85d09e6df99
Filipe Badaró <badaro@gmail.com>
2024-11-21 20:47:58 -0300

    Adjusted scryfall query so split cards also accept the front side

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 var config = {};
 config.rootUrl = "https://badaro.github.io/validator/";
-config.transformUrl = "https://api.scryfall.com/cards/search?order=cmc&q=%28is%3Aflip+or+is%3Adfc+or+is%3Aadventure%29+-is%3Aextra+-is%3Adigital+is%3Afirstprint";
+config.transformUrl = "https://api.scryfall.com/cards/search?order=cmc&q=(is%3Aflip+or+is%3Adfc+or+is%3Aadventure+or+is%3Asplit)+-is%3Aextra+-is%3Adigital+is%3Afirstprint";
 config.formats = [
   { "name": "2015 Modern", "key": "2015modern", "datafile":"formats/2015modern.json" },
   { "name": "Pre FIRE Modern", "key": "prefiremodern", "datafile":"formats/prefiremodern.json" }
